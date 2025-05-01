718cffaeb18fa2a0456ce377f7687c75f2403580
Dev Mohamad Ashraf <135912432+SupidoRonin@users.noreply.github.com>
2025-05-02 02:44:10 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,48 @@
 const hamMenu = document.querySelector('.Menu-icon');
 
-const offScreenMenu = document.querySelector('.Navigation-bar')
+const offScreenMenu = document.querySelector('.Navigation-bar');
+
+const cursordot = document.querySelector(".cursor-dot");
+const cursoroutline = document.querySelector(".cursor-outline");
+
+window.addEventListener("mousemove", (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursordot.style.left = `${posX}px`;
+    cursordot.style.top = `${posY}px`;
+
+    cursoroutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`,
+    }, {duration: 50, fill: "forwards"})
+
+    if (e.target.id === "dot"){
+        cursordot.classList.add("active");
+    }
+    else{
+        cursordot.classList.remove("active");
+    }
+
+    if (e.target.id === "outline"){
+        cursoroutline.classList.add("active");
+    }
+    else{
+        cursoroutline.classList.remove("active");
+    }
+});
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    })
+});
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('#Menu-icon');
     offScreenMenu.classList.toggle('active');
-})
+});
