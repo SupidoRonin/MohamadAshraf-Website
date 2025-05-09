8ab6df35c059a8a3a9e55c47c8ce098267637a79
Dev Mohamad Ashraf <135912432+SupidoRonin@users.noreply.github.com>
2025-05-09 17:28:11 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,6 +5,8 @@ const offScreenMenu = document.querySelector('.Navigation-bar');
 const cursordot = document.querySelector(".cursor-dot");
 const cursoroutline = document.querySelector(".cursor-outline");
 
+const input = document.querySelector("#phone");
+
 window.addEventListener("mousemove", (e) => {
     const posX = e.clientX;
     const posY = e.clientY;
@@ -46,3 +48,14 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('#Menu-icon');
     offScreenMenu.classList.toggle('active');
 });
+
+window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: callback => {
+      fetch('https://ipapi.co/json')
+        .then(res => res.json())
+        .then(data => callback(data.country_code))
+        .catch(() => callback('us'));
+    },
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js"
+  });
