4e46f60892dfc12fc9c2ec5cab638c722e4d66de
Dev Mohamad Ashraf <135912432+SupidoRonin@users.noreply.github.com>
2026-01-31 22:20:45 +0200

    Delete My Projects directory

--- My Projects/Games/Pages/Game/Script.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector('.Menu-icon');
-
-const offScreenMenu = document.querySelector('.Navigation-bar')
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('#Menu-icon');
-    offScreenMenu.classList.toggle('active');
-})
